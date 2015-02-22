1ba2971aaaec2b05ba916e2755c84f90f76d0966
Anton <anton.estum@gmail.com>
2015-02-22 06:16:07 +0300

    Update coffeelint workflow

--- Support/lib/validate_on_save/validators/coffee.rb
+++ Support/lib/validate_on_save/validators/coffee.rb
@@ -1,15 +1,20 @@
 module VOS
   module Validate
     def self.coffee
-      binary = ENV['TM_COFFEESLINT'] ||= "coffeelint"
-      filepath = ENV['TM_FILEPATH']
-      VOS.output({
-        :info => "Running syntax check with CoffeeScript lint\n",
-        :result => `"#{binary}" "#{filepath}" 2>&1`.sub(/^Error.*\.coffee, /, ''),
-        :match_ok => /0 error\(s\)\, /i, # ignore warnings
-        :match_line => /line (\d+)/i,
-        :lang => "CoffeeScript"
-      })
+      binary = ENV['TM_COFFEELINT'] ||= "coffeelint"
+      options = (ENV['TM_COFFEELINT_OPTS'] || "--color=never").split(/\s+/)
+
+      IO.popen [binary, *options, ENV['TM_FILEPATH'], :err => [:child, :out]], "r" do |io|
+        result = io.read.gsub(/^(.+) #(\d+): /, 'Line \2: ')
+
+        VOS.output({
+          :info       => "Running syntax check with CoffeeScript lint\n",
+          :result     => result,
+          :match_ok   => /\b0 errors\b.*\b0 warnings\b/i, # ignore warnings
+          :match_line => /line (\d+): /i,
+          :lang       => "CoffeeScript"
+        })
+      end
     end
   end
 end
